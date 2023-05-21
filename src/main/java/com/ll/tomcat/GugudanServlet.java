81dc332a5650fa913aee1cf262d3c74fb40e71b8
yurrrrri <yu_ri_son@naver.com>
2023-05-21 21:14:45 +0900

    model2 방식으로 구구단 구현

--- src/main/java/com/ll/tomcat/GugudanServlet.java
+++ src/main/java/com/ll/tomcat/GugudanServlet.java
@@ -1,22 +1,27 @@
 package com.ll.tomcat;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 @WebServlet("/gugudan")
 public class GugudanServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Rq rq = new Rq(req, resp);
 
         int dan = rq.getIntParam("dan", 9);
         int limit = rq.getIntParam("limit", 9);
 
-        rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
-        for(int i=1; i<=limit; i++){
-            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
-        }
+        req.setAttribute("dan", dan);
+        req.setAttribute("limit", limit);
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/gugudan2.jsp");
+        requestDispatcher.forward(req, resp);
     }
 }
--- src/webapp/gugudan2.jsp
+++ src/webapp/gugudan2.jsp
@@ -1,9 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ page import="com.ll.tomcat.Rq" %>
 <%
-Rq rq = new Rq(request, response);
-int dan = rq.getIntParam("dan", 9);
-int limit = rq.getIntParam("limit", 9);
+int dan = (int)request.getAttribute("dan");
+int limit = (int)request.getAttribute("limit");
 %>
 
 <h1><%=dan%>단</h1>
