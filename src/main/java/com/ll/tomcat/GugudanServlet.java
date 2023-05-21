c14de72ed50634c1743fb120da60b1d9f1986eb1
yurrrrri <yu_ri_son@naver.com>
2023-05-21 20:59:19 +0900

    JSP방식으로 구구단 구현

--- src/main/java/com/ll/tomcat/GugudanServlet.java
+++ src/main/java/com/ll/tomcat/GugudanServlet.java
@@ -1,17 +1,14 @@
 package com.ll.tomcat;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-
 @WebServlet("/gugudan")
 public class GugudanServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         Rq rq = new Rq(req, resp);
 
         int dan = rq.getIntParam("dan", 9);
